7898d29d9f508b57a97bf0836dafc8b067a50f8b
Mohammad Anees <mohammad.irock@gmail.com>
2018-02-15 19:51:06 -0600

    Make carousel on login component work

--- V2/anees-photos/src/app/login/login.component.ts
+++ V2/anees-photos/src/app/login/login.component.ts
@@ -8,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   private carouselImagePath: string;
+  private carouselIndex = 1;
 
   constructor() {
-    this.carouselImagePath = '/assets/images/kiwi1.JPG';
+    this.carouselImagePath = '/assets/images/login_pic_1.JPG';
   }
 
   ngOnInit() {
+  }
+
+  private changeImage(newIndex: number){
+    if (newIndex < 1) { newIndex = 3; }
+    if (newIndex > 3) { newIndex = 1; }
+
+    this.carouselIndex = newIndex;
+    const newImagePath = '/assets/images/login_pic_' + this.carouselIndex + '.JPG';
+    this.carouselImagePath = newImagePath;
   }
 
 }
